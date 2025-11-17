a8fb37eff73e0cfd81eb2bd249b417bdd1e48461
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2025-11-17 15:52:42 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,87 @@
+#10-misol
+s = "0123456789"
+print(s)
+
+toza = s[::2]
+print(toza)
+
+
+#11-misol
+s = "abcdefgh"
+print(s)
+
+toza = s[1::2]
+print(toza)
+
+
+#12-misol
+s = "QWERTYUIOP"
+print(s)
+
+toza = s[::-1]
+toza2 = s[::2]
+print(toza2)
+
+
+#13-misol
+s = "abracadabra"
+print(s)
+
+toza = s[4:8]
+print(toza)
+
+
+#14-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[3:3]
+print(toza)
+
+
+#15-misol
+s = "GameOverMission"
+print(s)
+
+toza = s[4:8]
+print(toza)
+
+
+#16-misol
+s = "123456789"
+print(s)
+
+toza = s[4::]
+toza2 = toza[::-1]
+print(toza2)
+
+
+#17-misol
+s = "indexing"
+print(s)
+
+toza = s[2:5]
+print(toza)
+
+
+#18-misol
+s = "slicingIsPowerful"
+print(s)
+
+toza = s[3:]
+print(toza)
+
+
+#19-misol
+s = "abcdefghij"
+print(s)
+
+toza = s[-5:-2]
+
+
+#20-misol
+s = "ABSDEFGHIJ"
+print(s)
+
+toza = s[-2:-8:-1]
+print(toza)
